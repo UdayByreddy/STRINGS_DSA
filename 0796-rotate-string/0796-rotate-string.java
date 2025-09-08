93f48a47755cf1a47e58884543fbd5be33d9eee4
UdayByreddy <139316540+UdayByreddy@users.noreply.github.com>
2025-09-08 11:46:29 +0530

    Time: 121 ms (7.98%), Space: 41.8 MB (29.56%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -1,14 +1,21 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
+        if(s.length()!=goal.length()){
+            return false;
+        }
+        
         if(s.equals(goal)){
+
             return true;
         }
-        for(int i=1;i<s.length();i++){
-            String tep = s.substring(i)+s.substring(0,i);
-            if(tep.equals(goal)){
-                return true;
-            }
-        }
-        return false;
+        // for(int i=1;i<s.length();i++){
+        //     String tep = s.substring(i)+s.substring(0,i);
+        //     if(tep.equals(goal)){
+        //         return true;
+        //     }
+        // }
+        // return false;
+
+        return(s+s).contains(goal);
     }
 }
