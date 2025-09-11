d0c7d1234c07ff2857db4dba205e11a227a6d9a9
UdayByreddy <139316540+UdayByreddy@users.noreply.github.com>
2025-09-11 11:09:45 +0530

    Time: 0 ms (100%), Space: 41.6 MB (66.56%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        int left=0;
+        int right=0;
+        for(char ch:s.toCharArray()){
+            if(ch=='(') left++;
+            else{
+                if(left>0) left--;
+                else{
+                    right++;
+                }
+            }
+        }
+        return Math.abs(left+right);
+    }
+}
